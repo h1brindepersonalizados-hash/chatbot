b2a0b4b1bb8709bf0a101e8d76caf813f02f5254
Seu Nome <seuemail@gmail.com>
2026-03-20 00:02:17 -0300

    removendo LocalAuth para railway

--- chatbot.js
+++ chatbot.js
@@ -8,7 +8,6 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 // CONFIGURAÇÃO DO CLIENTE
 // =====================================
 const client = new Client({
-  authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
